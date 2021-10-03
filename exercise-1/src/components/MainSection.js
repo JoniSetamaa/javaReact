b2a5dfd1df31e23e3b820b00b7eab8d7de1e0e65
Joni <t0sejo01@students.oamk.fi>
2021-10-03 21:33:22 +0300

    Almost done

--- exercise-1/src/components/MainSection.js
+++ exercise-1/src/components/MainSection.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Taiwan from './../images/Taiwan.jpg';
+import styles from './MainSection.module.css';
 
 export default function MainSection() {
     return (
-
-        <div className="container">
-
+        <div className={ styles.container }>
+            <img src={Taiwan} alt="Taiwan"/>
+            <div className={ styles.header }><h2>Taiwanissa tapahtuu</h2></div>
+            <div className={ styles.footer }><p>Ulkomaat 19:11</p></div>
         </div>
-            )
+    )
 }
--- exercise-1/src/components/SideSection.js
+++ exercise-1/src/components/SideSection.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SideSection.module.css';
 
 export default function SideSection() {
-    return (
-        <div>
-             
-        </div>
+    return(
+        <div> 
+       
+
+       </div>
     )
 }
